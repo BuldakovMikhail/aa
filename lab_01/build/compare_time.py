878c062671d90261660a215898d4b6da0831f239
Buldakov Mikhail <mishabul2003@gmail.com>
2023-09-28 20:36:55 +0300

    research part added

---
+++ lab_01/build/compare_time.py
@@ -0,0 +1,132 @@
+from time import process_time_ns
+import matplotlib.pyplot as plt
+
+from algorithms import *
+
+
+def generate_strings(string_length=5):
+    s1 = ""
+    s2 = ""
+
+    for _ in range(string_length):
+        s1 += "a"
+        s2 += "b"
+
+    return s1, s2
+
+
+def time_analysis(function, iterations, length):
+    string_1, string_2 = generate_strings(length)
+
+    time_start = process_time_ns()
+
+    for _ in range(iterations):
+        function(string_1, string_2)
+
+    time_stop = process_time_ns()
+
+    return (time_stop - time_start) / iterations
+
+
+def print_measurement_res(
+    sizes,
+    time_lev_table,
+    time_dam_lev_table,
+    time_dam_lev_recursion,
+    time_dam_lev_recursion_cash,
+):
+    print("-" * 136)
+
+    print(
+        "|{:^10s}|{:^30s}|{:^30s}|{:^30s}|{:^30s}|".format(
+            "Длина",
+            "Левенштейн (и)",
+            "Дамерау-Левенштейн (и)",
+            "Дамерау-Левенштейн (р)",
+            "Дамерау-Левенштейн (рк)",
+        )
+    )
+
+    print("-" * 136)
+
+    for i in range(len(sizes)):
+        print(
+            "|{:^10d}|{:^30.2e}|{:^30.2e}|{:^30.2e}|{:^30.2e}|".format(
+                sizes[i],
+                time_lev_table[i],
+                time_dam_lev_table[i],
+                time_dam_lev_recursion[i],
+                time_dam_lev_recursion_cash[i],
+            )
+        )
+
+    print("-" * 136)
+
+
+def build_graph(
+    sizes,
+    time_lev_table,
+    time_dam_lev_table,
+    time_dam_lev_recursion,
+    time_dam_lev_recursion_cash,
+):
+    fig1 = plt.figure(figsize=(10, 7))
+    plot = fig1.add_subplot()
+    plot.plot(sizes, time_lev_table, label="Левенштейн (матричная)", marker=">")
+    plot.plot(
+        sizes, time_dam_lev_table, label="Дамерау-Левенштейн (матричная)", marker="+"
+    )
+    plot.plot(
+        sizes,
+        time_dam_lev_recursion,
+        label="Дамерау-Левенштейн (рекурсивная)",
+        marker="v",
+    )
+    plot.plot(
+        sizes,
+        time_dam_lev_recursion_cash,
+        label="Дамерау-Левенштейн (рекурсивная с кешем)",
+        marker="D",
+    )
+
+    plt.legend()
+    plt.grid()
+    plt.title("Сравнение алгоритмом по времени")
+    plt.ylabel("Затраченное время (с)")
+    plt.xlabel("Длина (кол-во букв)")
+    plt.yscale("log")
+
+    plt.show()
+
+
+def compare_time():
+    time_lev_table = []
+    time_dam_lev_table = []
+    time_dam_lev_recursion_cash = []
+    time_dam_lev_recursion = []
+
+    sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    for n in sizes:
+        time_lev_table.append(time_analysis(levenstein, 2000, n))
+        time_dam_lev_table.append(time_analysis(damerau_levenstein_iter, 2000, n))
+        time_dam_lev_recursion_cash.append(
+            time_analysis(damerau_levenstein_rec_cash, 1000, n)
+        )
+        time_dam_lev_recursion.append(time_analysis(damerau_levenstein_rec, 1000, n))
+
+    print_measurement_res(
+        sizes,
+        time_lev_table,
+        time_dam_lev_table,
+        time_dam_lev_recursion,
+        time_dam_lev_recursion_cash,
+    )
+
+    build_graph(
+        sizes,
+        time_lev_table,
+        time_dam_lev_table,
+        time_dam_lev_recursion,
+        time_dam_lev_recursion_cash,
+    )
--- lab_01/build/main.py
+++ lab_01/build/main.py
@@ -1,121 +1,99 @@
-def m(a, b):
-    return 0 if a == b else 1
+from algorithms import *
+
+from compare_time import *
 
 
-def levenstein(s1, s2):
-    matrix = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if i == 0 and j == 0:
-                matrix[i][j] = 0
-            elif i > 0 and j == 0:
-                matrix[i][j] = i
-            elif j > 0 and i == 0:
-                matrix[i][j] = j
-            else:
-                matrix[i][j] = min([matrix[i][j - 1] + 1, matrix[i-1]
-                                   [j] + 1, matrix[i - 1][j - 1] + m(s1[i-1], s2[j-1])])
-    return matrix[-1][-1]
-
-
-def damerau_levenstein_iter(s1, s2):
-    d = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
-    for i in range(len(d)):
-        for j in range(len(d[0])):
-            if i == 0 and j == 0:
-                d[i][j] = 0
-            elif i > 0 and j == 0:
-                d[i][j] = i
-            elif j > 0 and i == 0:
-                d[i][j] = j
-            elif i > 1 and j > 1 and s1[i-1] == s2[j-2] and s1[i-2] == s2[j-1]:
-                d[i][j] = min([d[i][j-1] + 1, d[i-1][j] + 1, d[i-1]
-                              [j-1] + m(s1[i-1], s2[j-1]), d[i-2][j-2] + 1])
-            else:
-                d[i][j] = min([d[i][j - 1] + 1, d[i-1]
-                                   [j] + 1, d[i - 1][j - 1] + m(s1[i-1], s2[j-1])])
-    return d[-1][-1]
-
-
-def damerau_levenstein_rec(s1, s2):
-    if len(s1) == 0 or len(s2) == 0:
-        return max(len(s1), len(s2))
-    temp = min([damerau_levenstein_rec(s1[:-1], s2) + 1,
-                damerau_levenstein_rec(s1, s2[:-1]) + 1,
-                damerau_levenstein_rec(s1[:-1], s2[:-1]) + m(s1[-1], s2[-1])])
-
-    if len(s1) > 1 and len(s2) > 1 and s1[-1] == s2[-2] and s1[-2] == s2[-1]:
-        temp = min(temp, damerau_levenstein_rec(s1[:-2], s2[:-2]) + 1)
-
-    return temp
-
-
-def damerau_levenstein_rec_cash(s1, s2):
-    temp = max(len(s1), len(s2)) + 1
-    d = [[-1] * (len(s2) + 1) for _ in range(len(s1) + 1)]
-
-    def dlrc(s1, s2):
-        if len(s1) == 0 or len(s2) == 0:
-            d[len(s1)][len(s2)] = max(len(s1), len(s2))
-            return d[len(s1)][len(s2)]
-
-        if d[len(s1[:-1])][len(s2)] >= 0:
-            dlr1 = d[len(s1[:-1])][len(s2)]
-        else:
-            # Случай если значение не в кеше, то вычисляем и "кешируем"
-            dlr1 = dlrc(s1[:-1], s2)
-            d[len(s1[:-1])][len(s2)] = dlr1
-
-        if d[len(s1)][len(s2[:-1])] >= 0:
-            dlr2 = d[len(s1)][len(s2[:-1])]
-        else:
-            # Случай если значение не в кеше, то вычисляем и "кешируем"
-            dlr2 = dlrc(s1, s2[:-1])
-            d[len(s1)][len(s2[:-1])] = dlr2
-
-        if d[len(s1[:-1])][len(s2[:-1])] >= 0:
-            dlr3 = d[len(s1[:-1])][len(s2[:-1])]
-        else:
-            # Случай если значение не в кеше, то вычисляем и "кешируем"
-            dlr3 = dlrc(s1[:-1], s2[:-1])
-            d[len(s1[:-1])][len(s2[:-1])] = dlr3
-
-        temp = min([dlr1 + 1,
-                    dlr2 + 1,
-                    dlr3 + m(s1[-1], s2[-1])])
-
-        if len(s1) > 1 and len(s2) > 1 and s1[-1] == s2[-2] and s1[-2] == s2[-1]:
-            if d[len(s1[:-2])][len(s2[:-2])] >= 0:
-                dlr4 = d[len(s1[:-2])][len(s2[:-2])]
-            else:
-                # Случай если значение не в кеше, то вычисляем и "кешируем"
-                dlr4 = dlrc(s1[:-2], s2[:-2])
-                d[len(s1[:-2])][len(s2[:-2])] = dlr4
-            temp = min(temp, dlr4 + 1)
-
-        d[len(s1)][len(s2)] = temp
-
-        return temp
-
-    return dlrc(s1, s2)
+# \hline
+# 		$\lambda$ & $\lambda$ & 0 & 0 & 0 & 0 \\
+# 		\hline
+# 		a & b & 1 & 1 & 1 & 1 \\
+# 		\hline
+# 		a & a & 0 & 0 & 0 & 0 \\
+# 		\hline
+# 		кот & скат & 2 & 2 & 2 & 2 \\
+# 		\hline
+# 		ab & ba & 2 & 1 & 1 & 1 \\
+# 		\hline
+# 		bba & abba & 1 & 1 & 1 & 1 \\
+# 		\hline
+# 		aboba & boba & 1 & 1 & 1 & 1 \\
+# 		\hline
+# 		abcdef & gh & 6 & 6 & 6 & 6 \\
+# 		\hline
 
 
 def test(s1, s2):
-    print('----------------------------------------------------')
-    print(f'Levenstein({s1}, {s2}): ', levenstein(s1, s2))
-    print(f'Damerau-Levenstein_iter({s1}, {s2}): ',
-          damerau_levenstein_iter(s1, s2))
-    print(f'Damerau-Levenstein_rec({s1}, {s2}): ',
-          damerau_levenstein_rec(s1, s2))
-    print(f'Damerau-Levenstein_rec_cash({s1}, {s2}): ',
-          damerau_levenstein_rec_cash(s1, s2))
-    print('----------------------------------------------------')
+    print("----------------------------------------------------")
+    print(f"Levenstein({s1}, {s2}): ", levenstein(s1, s2))
+    print(f"Damerau-Levenstein_iter({s1}, {s2}): ", damerau_levenstein_iter(s1, s2))
+    print(f"Damerau-Levenstein_rec({s1}, {s2}): ", damerau_levenstein_rec(s1, s2))
+    print(
+        f"Damerau-Levenstein_rec_cash({s1}, {s2}): ",
+        damerau_levenstein_rec_cash(s1, s2),
+    )
+    print("----------------------------------------------------")
+
+
+def show_menu():
+    print(
+        """
+    
+    1 - Расстояние Левенштейна (итеративно)
+    2 - Расстояние Дамерау-Левенштейна (итеративно)
+    3 - Расстояние Дамерау-Левенштейна (рекурсивно)
+    4 - Расстояние Дамерау-Левенштейна (рекурсивно с кешированием)
+    5 - Вывести результаты тестов
+    6 - Замер времени
+    0 - Выход
+
+        """
+    )
 
 
 def main():
-    test('ab', 'ba')
-    test('vvvvvc', 'bbbbbc')
+    show_menu()
+    while x := int(input("Выберите опцию: ")):
+        if x == 0:
+            break
+        elif x == 5:
+            tests = [
+                ("", ""),
+                ("a", "b"),
+                ("a", "a"),
+                ("кот", "скат"),
+                ("ab", "ba"),
+                ("bba", "abba"),
+                ("aboba", "boba"),
+                ("abcdef", "gh"),
+            ]
+
+            for t in tests:
+                test(*t)
+            # test("ab", "ba")
+            # test("vvvvvc", "bbbbbc")
+        elif x == 1:
+            s1 = input("Введите строку 1: ")
+            s2 = input("Введите строку 2: ")
+            print(f"Расстояние = {levenstein(s1, s2)}")
+        elif x == 2:
+            s1 = input("Введите строку 1: ")
+            s2 = input("Введите строку 2: ")
+            print(f"Расстояние = {damerau_levenstein_iter(s1, s2)}")
+        elif x == 3:
+            s1 = input("Введите строку 1: ")
+            s2 = input("Введите строку 2: ")
+            print(f"Расстояние = {damerau_levenstein_rec(s1, s2)}")
+        elif x == 4:
+            s1 = input("Введите строку 1: ")
+            s2 = input("Введите строку 2: ")
+            print(f"Расстояние = {damerau_levenstein_rec_cash(s1, s2)}")
+        elif x == 6:
+            compare_time()
+        else:
+            print("Неверная опция")
+
+        show_menu()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
