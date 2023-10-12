df292ed9e2e3a1e534e7d58274f768cca980424c
Buldakov Mikhail <mishabul2003@gmail.com>
2023-10-12 17:47:49 +0300

    algo schemes updated

--- lab_01/build/algorithms.py
+++ lab_01/build/algorithms.py
@@ -73,42 +73,26 @@ def damerau_levenstein_rec(s1, s2):
 def damerau_levenstein_rec_cash(s1, s2):
     d = [[-1] * (len(s2) + 1) for _ in range(len(s1) + 1)]
 
+    def get_value(s1, s2):
+        if d[len(s1)][len(s2)] == -1:
+            d[len(s1)][len(s2)] = dlrc(s1, s2)
+
+        return d[len(s1)][len(s2)]
+
     def dlrc(s1, s2):
         if len(s1) == 0 or len(s2) == 0:
             d[len(s1)][len(s2)] = max(len(s1), len(s2))
             return d[len(s1)][len(s2)]
 
-        if d[len(s1[:-1])][len(s2)] >= 0:
-            dlr1 = d[len(s1[:-1])][len(s2)]
-        else:
-            # Случай если значение не в кеше, то вычисляем и "кешируем"
-            dlr1 = dlrc(s1[:-1], s2)
-            d[len(s1[:-1])][len(s2)] = dlr1
+        dlr1 = get_value(s1[:-1], s2) + 1
+        dlr2 = get_value(s1, s2[:-1]) + 1
+        dlr3 = get_value(s1[:-1], s2[:-1]) + m(s1[-1], s2[-1])
 
-        if d[len(s1)][len(s2[:-1])] >= 0:
-            dlr2 = d[len(s1)][len(s2[:-1])]
-        else:
-            # Случай если значение не в кеше, то вычисляем и "кешируем"
-            dlr2 = dlrc(s1, s2[:-1])
-            d[len(s1)][len(s2[:-1])] = dlr2
-
-        if d[len(s1[:-1])][len(s2[:-1])] >= 0:
-            dlr3 = d[len(s1[:-1])][len(s2[:-1])]
-        else:
-            # Случай если значение не в кеше, то вычисляем и "кешируем"
-            dlr3 = dlrc(s1[:-1], s2[:-1])
-            d[len(s1[:-1])][len(s2[:-1])] = dlr3
-
-        temp = min([dlr1 + 1, dlr2 + 1, dlr3 + m(s1[-1], s2[-1])])
+        temp = min([dlr1, dlr2, dlr3])
 
         if len(s1) > 1 and len(s2) > 1 and s1[-1] == s2[-2] and s1[-2] == s2[-1]:
-            if d[len(s1[:-2])][len(s2[:-2])] >= 0:
-                dlr4 = d[len(s1[:-2])][len(s2[:-2])]
-            else:
-                # Случай если значение не в кеше, то вычисляем и "кешируем"
-                dlr4 = dlrc(s1[:-2], s2[:-2])
-                d[len(s1[:-2])][len(s2[:-2])] = dlr4
-            temp = min(temp, dlr4 + 1)
+            dlr4 = get_value(s1[:-2], s2[:-2]) + 1
+            temp = min(temp, dlr4)
 
         d[len(s1)][len(s2)] = temp
 
--- lab_01/docs/inc/lst/dameraylevreccash.py
+++ lab_01/docs/inc/lst/dameraylevreccash.py
@@ -1,38 +1,26 @@
 def damerau_levenstein_rec_cash(s1, s2):
     d = [[-1] * (len(s2) + 1) for _ in range(len(s1) + 1)]
 
+    def get_value(s1, s2):
+        if d[len(s1)][len(s2)] == -1:
+            d[len(s1)][len(s2)] = dlrc(s1, s2)
+
+        return d[len(s1)][len(s2)]
+
     def dlrc(s1, s2):
         if len(s1) == 0 or len(s2) == 0:
             d[len(s1)][len(s2)] = max(len(s1), len(s2))
             return d[len(s1)][len(s2)]
 
-        if d[len(s1[:-1])][len(s2)] >= 0:
-            dlr1 = d[len(s1[:-1])][len(s2)]
-        else:
-            dlr1 = dlrc(s1[:-1], s2)
-            d[len(s1[:-1])][len(s2)] = dlr1
+        dlr1 = get_value(s1[:-1], s2) + 1
+        dlr2 = get_value(s1, s2[:-1]) + 1
+        dlr3 = get_value(s1[:-1], s2[:-1]) + m(s1[-1], s2[-1])
 
-        if d[len(s1)][len(s2[:-1])] >= 0:
-            dlr2 = d[len(s1)][len(s2[:-1])]
-        else:
-            dlr2 = dlrc(s1, s2[:-1])
-            d[len(s1)][len(s2[:-1])] = dlr2
-
-        if d[len(s1[:-1])][len(s2[:-1])] >= 0:
-            dlr3 = d[len(s1[:-1])][len(s2[:-1])]
-        else:
-            dlr3 = dlrc(s1[:-1], s2[:-1])
-            d[len(s1[:-1])][len(s2[:-1])] = dlr3
-
-        temp = min([dlr1 + 1, dlr2 + 1, dlr3 + m(s1[-1], s2[-1])])
+        temp = min([dlr1, dlr2, dlr3])
 
         if len(s1) > 1 and len(s2) > 1 and s1[-1] == s2[-2] and s1[-2] == s2[-1]:
-            if d[len(s1[:-2])][len(s2[:-2])] >= 0:
-                dlr4 = d[len(s1[:-2])][len(s2[:-2])]
-            else:
-                dlr4 = dlrc(s1[:-2], s2[:-2])
-                d[len(s1[:-2])][len(s2[:-2])] = dlr4
-            temp = min(temp, dlr4 + 1)
+            dlr4 = get_value(s1[:-2], s2[:-2]) + 1
+            temp = min(temp, dlr4)
 
         d[len(s1)][len(s2)] = temp
 
